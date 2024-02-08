6d279508e988e4c16ccfc83566e7b97534a59f02
Dan Mossesgeld <golez.sf@gmail.com>
2024-02-08 13:28:58 +0800

    print

--- main/modules/invoice/print_invoice.php
+++ main/modules/invoice/print_invoice.php
@@ -58,7 +58,7 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 <meta charset="utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="icon" type="image/x-icon" href="../images/conogas.png">
+<link rel="icon" type="image/x-icon" href="../images/sgc.png">
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900' type='text/css'>
 <title>Invoice Print</title>
 <meta name="author" content="harnishdesign.net">
@@ -117,7 +117,7 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 <body>
 <!-- Container -->
 <div class="content-wrapper">
-    <img src="../images/conogas.png">
+    <img src="../images/sgc.png">
     <!-- Content Header (Page header) -->
     <div class="content-header">
         <div class="container-fluid">
