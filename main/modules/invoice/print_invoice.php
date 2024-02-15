c561d3e662cca405fa33320699f9224a2ba2bce4
WTF AM I DOING? <84323037+kenthoughts15@users.noreply.github.com>
2024-02-15 11:56:59 +0800

    Update print_invoice.php

--- main/modules/invoice/print_invoice.php
+++ main/modules/invoice/print_invoice.php
@@ -162,11 +162,11 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>
-                    <td style='text-align: left; padding-bottom: 2px; white-space: nowrap;'>" . $row["description"] . "</td>
-                    <td style='padding-left: 260px; padding-bottom: 2px;'>" . $row["quantity"] . "</td>
-                    <td style='padding-bottom: 2px; white-space: nowrap;'>" . $row["uom"] . "</td>
-                    <td style='text-align: right; padding-right: 58px; padding-bottom: 2px'>" . $row["rate"] . "</td>
-                    <td style='text-align: right; padding-bottom: 2px'>" . $row["amount"] . "</td>
+                    <td style='text-align: left; padding-bottom: 2.4px; white-space: nowrap;'>" . $row["description"] . "</td>
+                    <td style='padding-left: 260px; padding-bottom: 2.4px;'>" . $row["quantity"] . "</td>
+                    <td style='padding-bottom: 2.4px; white-space: nowrap;'>" . $row["uom"] . "</td>
+                    <td style='text-align: right; padding-right: 58px; padding-bottom: 2.4px;'>" . $row["rate"] . "</td>
+                    <td style='text-align: right; padding-bottom: 2.4px;'>" . $row["amount"] . "</td>
                 </tr>";
             }
         } else {
